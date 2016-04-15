67ec391b4d47e79daf3052c392dc55d8331f52f4
Eric McCormick <edm00se@gmail.com>
2016-04-15 11:29:26 -0500

    added compression

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ var express = require('express');
 var app = express();
 var conf = require('./config');
 var ejs = require('ejs');
+var compression = require('compression');
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 var port = (process.env.VCAP_APP_PORT || process.env.PORT || 3000);
 
 //enables and instantiates express-toobusy, which keeps it from melting under HIGH pressure
 app.use(require('express-toobusy')());
+
+// compress responses
+app.use(compression());
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
